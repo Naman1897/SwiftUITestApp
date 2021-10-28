01775fdde70b42c68fe8506ccdac6d52e91aeae2
Naman Singh <naman1897@icloud.com>
2021-10-28 20:41:28 +0530

    Initial Commit

---
+++ SwiftUITestApp/SwiftUITestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestAppApp.swift
+//  SwiftUITestApp
+//
+//  Created by Naman Singh on 28/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
